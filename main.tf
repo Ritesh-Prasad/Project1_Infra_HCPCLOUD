1827f404303915ef7599bbfe0ac7334142b77a99
Ritesh-Prasad <ritesh010996@gmail.com>
2026-02-24 13:19:23 +0530

    second commit

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
     region = "ap-south-1"
 }
-resource "aws_instance" "server" {
+resource "aws_instance" "one" {
     count = 3
-    ami = "019715e0d74f695be"
+    ami = "ami-0ff91eb5c6fe7cc86"
     instance_type = "c7i-flex.large"
     vpc_security_group_ids = ["sg-0d9bbcd4e65b3ed2d"]
     key_name = "amazon"
